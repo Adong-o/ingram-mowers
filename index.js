7e110f0aecf17477edcbc16074a39d8860371b24
Daucro <142437961+Daucro@users.noreply.github.com>
2023-08-23 18:07:24 -0500

    Tabs corrected
    additional work done on header background

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 function openInfo(evt, tabName) {
-    //declaring variables
-    var i, tabcontent, tablinks;
+  //declaring variables
+  var i, tabcontent, tablinks;
 
-    //get elements with tabcontent class and hide
+  //get elements with tabcontent class and hide
 
-tabcontent = document.getElementsByClassName("tabcontent");
-for (i=0; i < tabcontent.length; i++){
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
+  }
+  //get elements with class tablinks and remove the active class
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+  //show current tab and add active class to it
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += "active";
 }
-    //get elements with class tablinks and remove the active class
-    tablinks= document.getElementsByClassName("tablinks");
-    for (i=0; i < tablinks.length; i++) {
-        tablinks[i].className=tablinks[i].className.replace("active","");
-    }
-    //show current tab and add active class to it
-    document.getElementById(tabName).style.display ="block";
-    evt.currentTarget.className += "active";
-
-}
-
+//will open home tab by default
+document.getElementById("defaultOpen").click();
